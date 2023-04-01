725d13989f6a9aa73e8b3549d7ad12e3ea7236d1
Archit Kumar <architkumar2705@gmail.com>
2023-04-01 17:51:49 +0100

    erm this wont rly work for physics. copy code for comp from math, and populate everything

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+import os, random
 
 
 views = Blueprint("views", __name__)
@@ -7,15 +8,76 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html")
 
-@views.route("/math", methods=["GET","POST"])
-def math():
-    if request.method == "GET":
+@views.route("/math/<arg>", methods=["GET","POST"])
+def math(arg):
+    if arg == "get":
         return render_template("math.html")
+    
+    params = arg.split("_")
 
-@views.route("/physics", methods=["GET","POST"])
-def physics():
-    if request.method == "GET":
+    directory = "website/static/math"
+    if params[0] == "shuffle":
+        ext = random.choice(["further","single"])
+        directory += f"/{ext}"
+        ext = random.choice(["pure","stats", "mech"])
+        directory += f"/{ext}"
+
+    elif params[1] == "shuffle":
+        ext = random.choice(["pure","stats","mech"])
+        directory += f"/{params[0]}/{ext}"
+
+    else:
+        directory += f"/{params[0]}/{params[1]}"
+
+    contents = os.listdir(directory)
+    try:
+        index = random.randint(1, len(contents)//2)
+    except:
+        return "directory empty"
+    question = f"{directory}/{index}q.png"
+    question = question[8:]
+    answer = f"{directory}/{index}a.png"
+    answer = answer[8:]
+    print(question, answer)
+    return render_template("QA.html", question=question, answer=answer)
+
+@views.route("/physics/<arg>", methods=["GET","POST"])
+def physics(arg):
+    if arg == "get":
         return render_template("physics.html")
+    
+    params = arg.split("_")
+
+    topics = {"paper1":["measurementsAndTheirErrors", "particlesAndRadiation", "waves", "mechAndMaterials", "electricity"],
+                   "paper2":["thermal", "fields", "nuclear"],
+                   "paper3":["practicalSkills","turningPoints"]}
+
+    directory = "website/static/physics"
+    if params[0] == "shuffle":
+        ext = random.choice(["paper1","paper2", "paper3"])
+        directory += f"/{ext}"
+        
+        ext = random.choice(topics[ext])
+        directory += f"/{ext}"
+
+    elif params[1] == "shuffle":
+        ext = random.choice(topics[params[0]])
+        directory += f"/{params[0]}/{ext}"
+
+    else:
+        directory += f"/{params[0]}/{params[1]}"
+
+    contents = os.listdir(directory)
+    try:
+        index = random.randint(1, len(contents)//2)
+    except:
+        return "directory empty"
+    question = f"{directory}/{index}q.png"
+    question = question[8:]
+    answer = f"{directory}/{index}a.png"
+    answer = answer[8:]
+    
+    return render_template("QA.html", question=question, answer=answer)
 
 @views.route("/compsci", methods=["GET","POST"])
 def compsci():
